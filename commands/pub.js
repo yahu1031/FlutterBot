63ad511a7f50ceea6b56c7744c101c01e6e73f82
yahu1031 <121003219@sastra.ac.in>
2020-10-16 23:28:54 +0530

    Fixed the pub issue #9 ⚡️

--- commands/pub.js
+++ commands/pub.js
@@ -25,16 +25,7 @@ module.exports = {
                     const data = await fetch(pubAPIurl).then(response => response.json())
                     if (data.packages.length === 0) return message.channel.send(notFoundMsg);
                     let result = [];
-                    if (data.packages.length == 1) {
-                        return message.channel.send(new Discord.MessageEmbed()
-                            .setColor('#2ECC71')
-                            .setTitle(`packages for search query ${args[0]}`)
-                            .addFields({
-                                name: data.packages[0].package,
-                                value: `https://pub.dev/packages/${data.packages[0].package}`
-                            }))
-                    };
-                    for (let i = 0; i < 10; i++) {
+                    for (let i = 0; i < data.packages.length; i++) {
                         const embededLinks = {
                             name: data.packages[i].package,
                             value: `https://pub.dev/packages/${data.packages[i].package}`
@@ -43,13 +34,14 @@ module.exports = {
                     }
                     const resultMsg = new Discord.MessageEmbed()
                         .setColor('#2ECC71')
-                        .setTitle(`packages for search query ${args[0]}`)
+                        .setTitle(`Pub packages for search ${args[0]}`)
                         .addFields(result)
                     if (result.length === 0) return message.channel.send(notFoundMsg);
                     message.channel.send(resultMsg);
                 } catch (error) {
                     return message.channel.send(error.message);
                 }
+                return
             }
             get_data(pubAPIurl);
             return;
