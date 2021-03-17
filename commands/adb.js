16d2feb4b7a1984dda9ef6f0d8676cc0e9670b3b
yahu1031 <121003219@sastra.ac.in>
2021-03-17 13:21:15 +0530

    Temp err commands💩

--- commands/adb.js
+++ commands/adb.js
@@ -11,7 +11,7 @@ module.exports = {
     execute(client, message, args) {
         let platform;
         switch (args[0]) {
-            case 'help':
+            case 'help' || '' || null:
                 return message.channel.send('**__Usage of adb command__** \n \n Use this command for adb download link for your prefered platform. \n > **__Eg__:** `!adb win`\n \nPlatforms to be passed with this command are `win`, `mac`, `linux`');
             case 'win':
                 platform = 'windows';
@@ -25,15 +25,14 @@ module.exports = {
             default:
                 return message.channel.send('Sorry, No such command found. try `!adb help`');
         }
-        console.log(platform);
         if (platform != 'undefined') {
             return message.reply(new Discord.MessageEmbed()
                 .setColor('#78c259')
-                .setAuthor('Download ADB installer')
+                .setAuthor('Download ADB Files')
                 .setThumbnail(androidimg)
                 .setDescription('**Android Debug Bridge**(adb) is a versatile command-line tool that lets you communicate with a device. This is recommended version to download.')
                 .addFields({
-                    name: 'ADB Installer',
+                    name: 'ADB Files',
                     value: `[**Download from here**](${adbLink + platform + '.zip'} "Download ADB for ${platform}")\n `,
                 }).setTimestamp().setFooter('NOTE: After downloading this, please extract the ZIP file and copy the folder to your root directory and add the folder to path.\n'));
         }
--- commands/errors.js
+++ commands/errors.js
@@ -3,6 +3,7 @@ const Discord = require('discord.js');
 const licenseimg = 'https://cdn.discordapp.com/attachments/756903745241088011/795996485179015198/file_type_license_1_1.png';
 const sdkimg = 'https://cdn.discordapp.com/attachments/756903745241088011/796080023227334666/th-removebg-preview.png';
 const pluginimg = 'https://cdn.discordapp.com/attachments/756903745241088011/796082433496973322/Group_40.png';
+const adbImg = 'https://cdn.discordapp.com/attachments/756903745241088011/821089644699320330/Android_operating_system-Robot-Logo.wine_1.png';
 
 module.exports = {
     name: 'error',
@@ -10,16 +11,15 @@ module.exports = {
     args: true,
     execute(client, message, args) {
         if (args[0] === 'help') {
-           return message.channel.send('**__Usage of adb command__** \n \n Use this command for adb download link for windows. \n > **__Eg__:** `!adb install`\n \nPlease do read the note of this command\'s result');
+            return message.channel.send('**__Usage of adb command__** \n \n Use this command for adb download link for windows. \n > **__Eg__:** `!adb install`\n \nPlease do read the note of this command\'s result');
         }
         else if (args[0] === 'license') {
             return message.reply(new Discord.MessageEmbed()
                 .setColor('#ff0000')
                 .setAuthor('Android license error')
                 .setThumbnail(licenseimg)
-                // .setDescription('**Android Debug Bridge**(adb) is a versatile command-line tool that lets you communicate with a device. This is recommended version to download.')
                 .addFields({
-                    name: '\n__Solution 1__\n',
+                    name: '\n__Solution__\n',
                     value: '\nAfter installing Flutter in the system, run `flutter doctor --android-license` this command in terminal/cmd. ' +
                         'Now you will be asked to accept few conditions/policies. Enter `y` on every question. And now you are ready to go.',
                 }).setTimestamp());
@@ -31,9 +31,11 @@ module.exports = {
                 .setThumbnail(sdkimg)
                 // .setDescription('**Android Debug Bridge**(adb) is a versatile command-line tool that lets you communicate with a device. This is recommended version to download.')
                 .addFields({
-                    name: '\n__Solution 1__\n',
-                    value: '\nAfter installing Flutter in the system, run `flutter doctor --android-license` this command in terminal/cmd. ' +
-                        'Now you will be asked to accept few conditions/policies. Enter `y` on every question. And now you are ready to go.',
+                    name: '\n__Solution__\n',
+                    value: 'If your Android studios was installed already, then\n' +
+                        '\n1. Open Android studios and go to settings.\n' +
+                        '\n2. In **Appearance & Behaviour > System Settings > Android SDK** choose one SDK platform(API 30/29 are recommended).\n' +
+                        '\n3. In the next tab **SDK Tools**, Check the box of *Android SDK Build-Tools, Android SDK Command-Line Tools, Android SDK Platform-Tools*.',
                 }).setTimestamp());
         }
         else if (args[0] === 'plugin') {
@@ -41,7 +43,6 @@ module.exports = {
                 .setColor('#ff0000')
                 .setAuthor('Plugins not Installed')
                 .setThumbnail(pluginimg)
-                // .setDescription('**Android Debug Bridge**(adb) is a versatile command-line tool that lets you communicate with a device. This is recommended version to download.')
                 .addFields({
                     name: 'Installed but issue?',
                     value: 'Currently we don\'t have any fix for this issue. Hope in future we will get a working solution for this. You can ignore it if you have already installed them.',
@@ -50,6 +51,18 @@ module.exports = {
                         name: 'Not Installed',
                         value: 'If you haven\'t installed them yet then follow the steps.\n\n' +
                             '**1.** Open settings(preferences)',
+                    }).setTimestamp());
+        }
+        else if (args[0] === 'adb') {
+            return message.reply(new Discord.MessageEmbed()
+                .setColor('#ff0000')
+                .setAuthor('ADB not found')
+                .setThumbnail(adbImg)
+                .setDescription('**Android Debug Bridge**(adb) is a versatile command-line tool that lets you communicate with a device. This is recommended version to download.')
+                .addFields(
+                    {
+                        name: 'ADB files exist but issue?',
+                        value: 'In this case, you just copy paste this path in your env path `C:\\Users\\%USERPROFILE%\\AppData\\Local\\Android\\Sdk\\platform-tools`(Windows only).',
                     }).setTimestamp());
         }
     },
